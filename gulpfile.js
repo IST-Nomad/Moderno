ac5f985b5f0cb4edf89433bc03c10822075bbdd3
Batr <ISTnomad@yandex.ru>
2023-02-12 16:38:14 +0800

    Header

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let cssmin = require("gulp-cssmin");
 
 gulp.task("sass", function () {
   return gulp
-    .src("app/scss/style.scss")
+    .src("app/scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(rename({ suffix: ".min" }))
     .pipe(
@@ -24,7 +24,7 @@ gulp.task("sass", function () {
 gulp.task("style", function () {
   return gulp
     .src([
-      "node_modules/normalize.css/normalize.css",
+      // "node_modules/normalize.css/normalize.css",
       "node_modules/slick-carousel/slick/slick.css",
       "node_modules/magnific-popup/dist/magnific-popup.css"
     ])
@@ -55,14 +55,16 @@ gulp.task("js", function () {
 
 gulp.task("browser-sync", function () {
   browserSync.init({
+      host: "localhost",
+      port: 3000,
     server: {
-      baseDir: "app/",
-    },
+      baseDir: "app/"
+    }
   });
 });
 
 gulp.task("watch", function () {
-  gulp.watch("app/scss/style.scss", gulp.parallel("sass"));
+  gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("app/*.html", gulp.parallel("html"));
   gulp.watch("app/js/*.js", gulp.parallel("js"));
 });
